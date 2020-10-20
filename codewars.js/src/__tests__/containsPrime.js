c073e6c9433ccaa9915e106e7a9f617e412304bd
Johan Svanström <svanis71@gmail.com>
2020-10-20 18:03:46 +0200

    Jest tests

---
+++ codewars.js/src/__tests__/containsPrime.js
@@ -0,0 +1,18 @@
+const isPrime = require('../containsPrime');
+
+test('optimusprime should return true', () => {
+  expect(isPrime('optimusprime')).toBeTruthy();
+});
+test('11 should return true', () => {
+  expect(isPrime('11')).toBeTruthy();
+});
+test('starPrime should return true', () => {
+  expect(isPrime('starPrime')).toBeTruthy();
+});
+test('11aagghh4 should return true', () => {
+  expect(isPrime('11aagghh4')).toBeTruthy();
+});
+
+test('4 should return false', () => {
+  expect(isPrime('4')).toBeFalsy();
+});
