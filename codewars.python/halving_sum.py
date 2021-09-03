cde9cc54f565f2768f7a00f996cd62ba3153270d
Johan Svanström <svanis71@gmail.com>
2021-09-03 08:35:23 +0200

    Halving sum

---
+++ codewars.python/halving_sum.py
@@ -0,0 +1,11 @@
+'''
+https://www.codewars.com/kata/5a58d46cfd56cb4e8600009d/train/python/61305d143b5bb90032b47a4b
+'''
+
+
+def halving_sum(n):
+    a = []
+    while n >= 1:
+        a.append(n)
+        n = int(n / 2)
+    return sum(a)
---
+++ codewars.python/tests/halving_sum_test.py
@@ -0,0 +1,13 @@
+import unittest
+
+from halving_sum import halving_sum
+
+
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(halving_sum(25), 47)
+        self.assertEqual(halving_sum(127), 247)
+
+
+if __name__ == '__main__':
+    unittest.main()
