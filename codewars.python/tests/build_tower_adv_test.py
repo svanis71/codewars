c9e05bee175e526352c0ee1cc754d72e064022e7
Johan Svanström <svanis71@gmail.com>
2021-09-09 08:49:14 +0200

    Build tower #2

---
+++ codewars.python/tests/build_tower_adv_test.py
@@ -0,0 +1,18 @@
+import unittest
+
+from build_tower_adv import tower_builder
+
+
+class MyTestCase(unittest.TestCase):
+    def test_simplest(self):
+        self.assertEqual(tower_builder(1, (1, 1)), ['*', ])
+
+    def test_something(self):
+        self.assertEqual(tower_builder(3, (4, 2)),
+                         ['        ****        ', '        ****        ', '    ************    ',
+                          '    ************    ', '********************', '********************'])
+
+
+# 2 * floors - 1
+if __name__ == '__main__':
+    unittest.main()
