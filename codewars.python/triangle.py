fda2219288dd7dc9996b62339a9fcd4dbacde5e0
Johan Svanström <svanis71@gmail.com>
2022-08-03 19:01:34 +0200

    Fix an issue

--- codewars.python/triangle.py
+++ codewars.python/triangle.py
@@ -4,4 +4,4 @@ def triangle(row):
     row = [0 if c == 'R' else 1 if c == 'G' else 2 for c in row]
     while len(row) > 1:
         row = [a if a == b else 3 - a - b for a, b in zip(row, row[1:])]
-    return row
+    return 'R' if row[0] == 0 else 'G' if row[0] == 1 else 'B'
