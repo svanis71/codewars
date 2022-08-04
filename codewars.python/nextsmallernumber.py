528f11e7638b8e8691cf0fc78bfaa65cf824f4a5
Johan Svanström <svanis71@gmail.com>
2022-08-04 07:41:09 +0200

    Next smaller number

--- codewars.python/nextsmallernumber.py
+++ codewars.python/nextsmallernumber.py
@@ -1,14 +1,20 @@
-import itertools
-
-
 def next_smaller(n):
-    digits = list(str(n))
-    curr_min = n
-    for i,c in enumerate(reversed(digits)):
-        [int(''.join(x)) for x in itertools.permutations(digits, len(digits))]
+    digits = [int(c) for c in str(n)]
+    if n < 21 or digits == sorted(digits):
+        return -1
+    result, first_smallest_pos, next_smallest_pos, next_smallest = [], len(digits) - 1, -1, -1
+    for i in range(len(digits) - 1, 0, -1):
+        if digits[i] < digits[i - 1]:
+            first_smallest_pos -= 1
+            break
+        first_smallest_pos -= 1
 
-    numbers = sorted(
-        [int(''.join(x)) for x in itertools.permutations(digits, len(digits)) if x[0] != '0' and x[0] <= digits[0]])
-    nextsmaller = [x for x in numbers if x < n]
-    return -1 if len(nextsmaller) == 0 else max(nextsmaller)
+    for i in range(first_smallest_pos, len(digits)):
+        if digits[i] < digits[first_smallest_pos] and digits[i] > next_smallest:
+            next_smallest = digits[i]
+            next_smallest_pos = i
 
+    digits[first_smallest_pos], digits[next_smallest_pos] = digits[next_smallest_pos], digits[first_smallest_pos]
+    tail = sorted(digits[first_smallest_pos + 1:], reverse=True)
+    result = [*digits[0:first_smallest_pos + 1], *tail]
+    return int(''.join(str(x) for x in result)) if result[0] > 0 else -1
--- codewars.python/tests/nextsmallernumber_test.py
+++ codewars.python/tests/nextsmallernumber_test.py
@@ -3,32 +3,38 @@ import unittest
 from nextsmallernumber import next_smaller
 
 
-class NextSmallerNumberTests(unittest.TestCase):
-    def test1(self):
+class NextSmallerTests(unittest.TestCase):
+    def test_next_smaller_1(self):
+        self.assertEqual(-1, next_smaller(3))
+
+    def test_next_smaller_2(self):
         self.assertEqual(next_smaller(907), 790)
 
-    def test2(self):
+    def test_next_smaller_970(self):
+        self.assertEqual(next_smaller(970), 907)
+
+    def test_next_smaller_3(self):
         self.assertEqual(next_smaller(531), 513)
 
-    def test3(self):
+    def test_next_smaller_4(self):
         self.assertEqual(next_smaller(511), 151)
 
-    def test4(self):
+    def test_next_smaller_5(self):
         self.assertEqual(next_smaller(135), -1)
 
-    def test5(self):
+    def test_next_smaller_6(self):
         self.assertEqual(next_smaller(2071), 2017)
 
-    def test6(self):
+    def test_next_smaller_7(self):
         self.assertEqual(next_smaller(414), 144)
 
-    def test7(self):
+    def test_next_smaller_8(self):
         self.assertEqual(next_smaller(123456798), 123456789)
 
-    def test8(self):
+    def test_next_smaller_9(self):
         self.assertEqual(next_smaller(123456789), -1)
 
-    def test9(self):
+    def test_next_smaller_10(self):
         self.assertEqual(next_smaller(1234567908), 1234567890)
 
 
