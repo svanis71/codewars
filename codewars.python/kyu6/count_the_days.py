fa1fe6461885234c4ad4086121f59fbcfa8b4e83
Johan Svanström <svanis71@gmail.com>
2022-09-05 16:41:17 +0200

    Count the days

---
+++ codewars.python/kyu6/count_the_days.py
@@ -0,0 +1,19 @@
+from datetime import datetime
+
+
+def get_today():
+    return datetime.now()
+
+
+def count_days(d: datetime) -> str:
+    '''
+    https://www.codewars.com/kata/5837fd7d44ff282acd000157/python
+    # of days should be rounded
+    :param d: A date
+    :return: Number of days to d
+    '''
+    delta = d - get_today()
+    days_left = delta.days + round(delta.seconds / 3600 / 24)
+    if delta.days == 0:
+        return 'Today is the day!'
+    return 'The day is in the past!' if days_left < 0 else f'{days_left} days'
---
+++ codewars.python/tests/kyu6/count_the_days_test.py
@@ -0,0 +1,40 @@
+import unittest
+from datetime import datetime
+from unittest.mock import patch
+
+from kyu6.count_the_days import count_days
+
+
+def get_today():
+    '''
+    Mock today
+    :return:
+    '''
+    return datetime(2022, 9, 5, 8, 11, 17, 178759)
+
+
+@patch("kyu6.count_the_days.get_today")
+class CountTheDaysTest(unittest.TestCase):
+    def test_past(self, today_mock):
+        today_mock.side_effect = get_today
+        self.assertEqual("The day is in the past!", count_days(datetime(2022, 9, 3, 18, 0)))
+
+    def test_future(self, today_mock):
+        today_mock.side_effect = get_today
+        self.assertEqual('1 days', count_days(datetime(2022, 9, 6, 18, 0)))
+
+    def test_future_2(self, today_mock):
+        today_mock.side_effect = get_today
+        self.assertEqual('2 days', count_days(datetime(2022, 9, 6, 23, 0)))
+
+    def test_today(self, today_mock):
+        today_mock.side_effect = get_today
+        self.assertEqual('Today is the day!', count_days(datetime(2022, 9, 5, 18, 0)))
+
+    def test_microseconds(self, today_mock):
+        today_mock.side_effect = get_today
+        self.assertEqual('Today is the day!', count_days(datetime(2022, 9, 5, 13, 11, 17, 178635)))
+
+
+if __name__ == '__main__':
+    unittest.main()
