ccf712128c4d3847a74001ba20dab32cc5c2ca00
Johan Svanström <svanis71@gmail.com>
2022-09-05 18:14:18 +0200

    Fixed a few pylint comments

--- codewars.python/build_tower.py
+++ codewars.python/build_tower.py
@@ -1,7 +1,7 @@
-"""
-https://www.codewars.com/kata/576757b1df89ecf5bd00073b/train/python
-"""
-
-
 def tower_builder(n_floors):
+    """
+    https://www.codewars.com/kata/576757b1df89ecf5bd00073b/train/python
+    :param n_floors: number of floors
+    :return: A tower
+    """
     return [('*' * i).center(n_floors * 2 - 1, ' ') for i in range(1, n_floors * 2, 2)]
--- codewars.python/build_tower_adv.py
+++ codewars.python/build_tower_adv.py
@@ -1,9 +1,10 @@
-"""
-https://www.codewars.com/kata/57675f3dedc6f728ee000256/python
-"""
-
-
 def tower_builder(n_floors, block_size):
+    """
+    https://www.codewars.com/kata/57675f3dedc6f728ee000256/python
+    :param n_floors: Floors
+    :param block_size: block size
+    :return:
+    """
     w, h = block_size
     tower = []
     for floor in range(1, n_floors + 1):
